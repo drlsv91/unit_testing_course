ef607557ecdc420d555d52606bf33990f79c2b40
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2019-12-13 18:23:40 +0100

    Mock Function

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,5 +1,6 @@
 const lib = require("../lib");
-
+const db = require("../db");
+const mail = require("../mail");
 describe("absolute", () => {
   it("should return a positive number if the input is positive", () => {
     const result = lib.absolute(1);
@@ -49,5 +50,24 @@ describe("registerUser", () => {
     const result = lib.registerUser("Victor");
     expect(result).toMatchObject({ username: "Victor" });
     expect(result.id).toBeGreaterThan(0);
+  });
+});
+describe("applyDiscount", () => {
+  db.getCustomerSync = function(customerId) {
+    console.log("Fake reading Customer");
+    return { id: customerId, points: 20 };
+  };
+  const order = { customerId: 1, totalPrice: 10 };
+  lib.applyDiscount(order);
+  expect(order.totalPrice).toBe(9);
+});
+describe("notifyCustomer", () => {
+  it("should send an email to the customer", () => {
+    db.getCustomerSync = jest.fn().mockReturnValue({ email: "a" });
+    mail.send = jest.fn();
+    lib.notifyCustomer({ customerId: 1 });
+    expect(mail.send).toHaveBeenCalled();
+    expect(mail.send.mock.calls[0][0]).toBe("a");
+    expect(mail.send.mock.calls[0][1]).toMatch(/order/);
   });
 });
